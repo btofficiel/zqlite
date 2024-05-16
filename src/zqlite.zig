cdac8f7cbc8b1a6ba5b81a26503cf3cc00766473
Bhaskar Thakur <bhaskar.dezire@gmail.com>
2024-05-16 15:51:39 +0530

    Add basic REPL

---
+++ src/zqlite.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const utils = struct {
+    usingnamespace @import("utils/repl.zig");
+};
+
+pub fn main() !void {
+    const inputBuffer = try utils.newInputBuffer();
+    const declaration = "ZQLite - A minimal SQLite implentation in Zig\n";
+    try utils.printOutput(declaration);
+    while (true) {
+        const prompt = "db > ";
+        try utils.printOutput(prompt);
+        try utils.readInput(inputBuffer);
+        if (inputBuffer.input) |input| {
+            if (std.mem.eql(u8, input, ".exit")) {
+                std.process.exit(0);
+            } else {
+                std.debug.print("Unrecognised command \n", .{});
+            }
+        }
+    }
+}
